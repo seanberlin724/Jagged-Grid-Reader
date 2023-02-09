e2e287ca21f293c35e5934af99dc1d909c12e251
Sean <seanberlin724@gmail.com>
2023-02-09 15:39:03 -0500

    4th- changed gameFileTester to "heads.txt"

--- GameFileTester.java
+++ GameFileTester.java
@@ -1,7 +1,7 @@
 public class GameFileTester {
     public static void main(String[] args) {
         // Create a new GridReader object
-        String filename = "Connect Four (winning state).txt";
+        String filename = "heads.txt";
         JaggedGridReader jaggedGrid = new JaggedGridReader(filename);
 
         // Display grid
