f835a0beb441a370b228a5af4b3ef800aa9da817
Sean <seanberlin724@gmail.com>
2023-02-10 20:27:45 -0500

    6th

--- GameFileTester.java
+++ GameFileTester.java
@@ -1,7 +1,7 @@
 public class GameFileTester {
     public static void main(String[] args) {
         // Create a new GridReader object
-        String filename = "Sudoku.txt";
+        String filename = "TicTacToe.txt";
         JaggedGridReader jaggedGrid = new JaggedGridReader(filename);
 
         // Display grid
